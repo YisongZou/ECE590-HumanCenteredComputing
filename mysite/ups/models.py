f661a05581ed778893c5d02816b3ec11a6518f66
Yisong Zou <yisongzou@yisong.zou@duke.edu>
2020-11-06 22:08:52 -0500

    Beifen

--- mysite/ups/models.py
+++ mysite/ups/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.postgres.fields import ArrayField
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Package(models.Model):
@@ -43,3 +44,14 @@ class Truck(models.Model):
     #  return self.truckId
 
 
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
